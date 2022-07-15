607501698cfd6446c9809d423ed904c6953e963a
sharath1954 <sharathkumarcg.99@gmail.com>
2022-07-16 03:36:34 +0530

    module 7

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -6,8 +6,10 @@ namespace WishList.Models
     {
         public int Id { get; set; }
 
+
         [Required]
         [MaxLength(50)]
         public string Description { get; set; }
+        public virtual ApplicationUser User { get; set; }
     }
 }
